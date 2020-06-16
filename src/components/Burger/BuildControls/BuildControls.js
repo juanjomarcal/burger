e7352c457533de54eb3f5e8fd237b6ee702e92bd
Juan José Martínez Calderón <juanjo.marcal@gmail.com>
2020-06-16 11:56:59 +0200

    Burger modal show

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -22,7 +22,12 @@ const BuildControls = (props) => (
         disabledInfo={props.disabledInfo}
       />
     ))}
-    <button className={classes.OrderButton} disabled={!props.purchasable}>ORDER NOW</button>
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.ordered}>
+      ORDER NOW
+    </button>
   </div>
 )
 
