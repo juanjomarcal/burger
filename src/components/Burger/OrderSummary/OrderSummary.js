6f4f4ae9ed74a70f3d1657b01eccff181f483115
Juan José Martínez Calderón <juanjo.marcal@gmail.com>
2020-06-16 13:01:58 +0200

    Total price added to Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,7 @@ const orderSummary = (props) => {
       <h3>Your order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientsSummary}</ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button btnType="Success" clicked={props.purchaseContinued}>CONFIRM</Button>
